39c46b39e67d42b649ccf15bbde8d9250f25614d
xfy <i@rua.plus>
2022-08-23 09:34:10 +0800

    Format

--- .config/nvim/after/plugin/prettier.rc.lua
+++ .config/nvim/after/plugin/prettier.rc.lua
@@ -1,16 +1,9 @@
 local status, prettier = pcall(require, "prettier")
-if (not status) then return end
+if (not status) then
+    return
+end
 
 prettier.setup {
-  bin = 'prettierd',
-  filetypes = {
-    "css",
-    "javascript",
-    "javascriptreact",
-    "typescript",
-    "typescriptreact",
-    "json",
-    "scss",
-    "less"
-  }
+    bin = 'prettierd',
+    filetypes = {"css", "javascript", "javascriptreact", "typescript", "typescriptreact", "json", "scss", "less"}
 }
--- .config/nvim/after/plugin/zen-mode.rc.lua
+++ .config/nvim/after/plugin/zen-mode.rc.lua
@@ -1,7 +1,10 @@
 local status, zenMode = pcall(require, "zen-mode")
-if (not status) then return end
+if (not status) then
+    return
+end
 
-zenMode.setup {
-}
+zenMode.setup {}
 
-vim.keymap.set('n', '<C-w>o', '<cmd>ZenMode<cr>', { silent = true })
+vim.keymap.set('n', '<C-w>o', '<cmd>ZenMode<cr>', {
+    silent = true
+})
