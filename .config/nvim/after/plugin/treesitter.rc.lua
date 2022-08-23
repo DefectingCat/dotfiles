6db214cc4e63a99202e3117062882e69c113a475
DefectingCat <i@defect.ink>
2022-08-23 09:34:10 +0800

    Format

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -1,30 +1,22 @@
 local status, ts = pcall(require, "nvim-treesitter.configs")
-if (not status) then return end
+if (not status) then
+    return
+end
 
 ts.setup {
-  highlight = {
-    enable = true,
-    disable = {},
-  },
-  indent = {
-    enable = true,
-    disable = {},
-  },
-  ensure_installed = {
-    "tsx",
-    "toml",
-    "json",
-    "yaml",
-    "css",
-    "html",
-    "lua",
-    "go",
-    "rust"
-  },
-  autotag = {
-    enable = true,
-  },
+    highlight = {
+        enable = true,
+        disable = {}
+    },
+    indent = {
+        enable = true,
+        disable = {}
+    },
+    ensure_installed = {"tsx", "toml", "json", "yaml", "css", "html", "lua", "go", "rust"},
+    autotag = {
+        enable = true
+    }
 }
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+local parser_config = require"nvim-treesitter.parsers".get_parser_configs()
+parser_config.tsx.filetype_to_parsername = {"javascript", "typescript.tsx"}
