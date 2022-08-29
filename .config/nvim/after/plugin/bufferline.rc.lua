eb0fcaf6c99a6d030c0de3e6581d07a78b36cc65
DefectingCat <i@defect.ink>
2022-08-29 14:01:02 +0800

    Add powerlevel10k theme

--- .config/nvim/after/plugin/bufferline.rc.lua
+++ .config/nvim/after/plugin/bufferline.rc.lua
@@ -6,7 +6,7 @@ bufferline.setup({
     mode = "buffers",
     separator_style = 'slant',
     always_show_bufferline = true,
-    show_buffer_close_icons = false,
+    show_buffer_close_icons = true,
     show_close_icon = false,
     color_icons = true
   },
