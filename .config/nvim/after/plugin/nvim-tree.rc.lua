c3ac19fc9b2e425dee68e6f9ff17bcaa21405e1b
DefectingCat <i@defect.ink>
2022-09-21 15:21:12 +0800

    update tree

--- .config/nvim/after/plugin/nvim-tree.rc.lua
+++ .config/nvim/after/plugin/nvim-tree.rc.lua
@@ -1,4 +1,9 @@
 local status, tree = pcall(require, 'nvim-tree')
 if (not status) then return end
 
-tree.setup()
+tree.setup({
+  update_focused_file = {
+        enable = true,
+        update_cwd = true,
+    },
+})
