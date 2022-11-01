10c42dee9b9c7455f053cd825cba6c3ff2f786f0
DefectingCat <i@defect.ink>
2022-11-01 09:05:58 +0800

    Update config

---
+++ .config/nvim/lua/xfy/autocmds.lua
@@ -0,0 +1,37 @@
+local myAutoGroup = vim.api.nvim_create_augroup("myAutoGroup", {
+	clear = true,
+})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- 修改lua/plugins.lua 自动更新插件
+autocmd("BufWritePost", {
+	group = myAutoGroup,
+	-- autocmd BufWritePost plugins.lua source <afile> | PackerSync
+	callback = function()
+		if vim.fn.expand("<afile>") == "lua/plugins.lua" then
+			vim.api.nvim_command("source lua/plugins.lua")
+			vim.api.nvim_command("PackerSync")
+		end
+	end,
+})
+
+-- Highlight on yank
+autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = myAutoGroup,
+	pattern = "*",
+})
+
+-- 用o换行不要延续注释
+autocmd("BufEnter", {
+	group = myAutoGroup,
+	pattern = "*",
+	callback = function()
+		vim.opt.formatoptions = vim.opt.formatoptions
+			- "o" -- O and o, don't continue comments
+			+ "r" -- But do continue when pressing enter.
+	end,
+})
--- .config/nvim/lua/xfy/plugins.lua
+++ .config/nvim/lua/xfy/plugins.lua
@@ -23,115 +23,144 @@ if not status then
 	return
 end
 
-return packer.startup(function(use)
-	use("wbthomason/packer.nvim")
+return packer.startup({
+	function(use)
+		use("wbthomason/packer.nvim")
 
-	use("Mofiqul/dracula.nvim") -- Color scheme
-	use("nvim-lualine/lualine.nvim") -- Statusline
-	use("nvim-lua/plenary.nvim") -- Common utilities
+		use("Mofiqul/dracula.nvim") -- Color scheme
+		use("nvim-lualine/lualine.nvim") -- Statusline
+		use("nvim-lua/plenary.nvim") -- Common utilities
 
-	-- terminal & window
-	use("szw/vim-maximizer") -- maximizs and restore current window
+		-- terminal & window
+		use("szw/vim-maximizer") -- maximizs and restore current window
 
-	-- text
-	-- use 'tpope/vim-surround'
+		-- text
+		-- use 'tpope/vim-surround'
 
-	-- autocompletion
-	use("hrsh7th/cmp-buffer") -- nvim-cmp source for buffer words
-	use("hrsh7th/cmp-nvim-lsp") -- nvim-cmp source for neovim's built-in LSP
-	use("hrsh7th/nvim-cmp") -- Completion
+		-- autocompletion
+		--
+		use("hrsh7th/nvim-cmp") -- Completion
+		use("hrsh7th/cmp-vsnip")
+		use("hrsh7th/cmp-path") -- { name = 'path' }
+		use("hrsh7th/cmp-cmdline") -- { name = 'cmdline' }
+		use("hrsh7th/cmp-nvim-lsp-signature-help") -- { name = 'nvim_lsp_signature_help' }
+		use("hrsh7th/cmp-buffer") -- nvim-cmp source for buffer words
+		use("hrsh7th/cmp-nvim-lsp") -- nvim-cmp source for neovim's built-in LSP
 
-	-- snippets
-	use("L3MON4D3/LuaSnip") -- snippet engine
-	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
-	use("rafamadriz/friendly-snippets")
+		-- snippets
+		use("L3MON4D3/LuaSnip") -- snippet engine
+		use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+		use("rafamadriz/friendly-snippets")
 
-	-- LSPs
-	use("neovim/nvim-lspconfig") -- LSP
-	use("onsails/lspkind-nvim") -- vscode-like pictograms
-	use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
-	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+		-- LSPs
+		use("neovim/nvim-lspconfig") -- LSP
+		use("onsails/lspkind-nvim") -- vscode-like pictograms
+		use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+		use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
 
-	-- formatting & linting
-	use("jose-elias-alvarez/null-ls.nvim") -- Use Neovim as a language server to inject LSP diagnos
-	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+		-- formatting & linting
+		use("jose-elias-alvarez/null-ls.nvim") -- Use Neovim as a language server to inject LSP diagnos
+		use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
 
-	-- managing & installing lsp servers, linters & formatters
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
+		-- managing & installing lsp servers, linters & formatters
+		use("williamboman/mason.nvim")
+		use("williamboman/mason-lspconfig.nvim")
 
-	-- treesitter configuration
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			require("nvim-treesitter.install").update({ with_sync = true })
-		end,
-	})
+		-- treesitter configuration
+		use({
+			"nvim-treesitter/nvim-treesitter",
+			run = function()
+				require("nvim-treesitter.install").update({ with_sync = true })
+			end,
+		})
 
-	use("kyazdani42/nvim-web-devicons") -- File icons
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = {
-			{ "nvim-telescope/telescope-live-grep-args.nvim" },
+		use("kyazdani42/nvim-web-devicons") -- File icons
+		use({
+			"nvim-telescope/telescope.nvim",
+			requires = {
+				{ "nvim-telescope/telescope-live-grep-args.nvim" },
+			},
+		})
+		-- use 'nvim-telescope/telescope-file-browser.nvim'
+		use("windwp/nvim-autopairs")
+		use("windwp/nvim-ts-autotag")
+		use("norcalli/nvim-colorizer.lua")
+		use({
+			"iamcco/markdown-preview.nvim",
+			run = function()
+				vim.fn["mkdp#util#install"]()
+			end,
+		})
+
+		use("akinsho/nvim-bufferline.lua")
+
+		-- git intergation
+		use("lewis6991/gitsigns.nvim")
+
+		-- File explorer
+		use({
+			"kyazdani42/nvim-tree.lua",
+			requires = {
+				"kyzadani42/nvim-web-devicons",
+			},
+			tag = "nightly",
+		})
+
+		-- Motion
+		use({
+			"phaazon/hop.nvim",
+			branch = "v2", -- optional but strongly recommended
+			config = function()
+				-- you can configure Hop the way you like here; see :h hop-config
+				require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
+			end,
+		})
+
+		use({ "glepnir/dashboard-nvim" })
+
+		-- multi cursor
+		use("mg979/vim-visual-multi")
+
+		-- Comment
+		use("terrortylor/nvim-comment")
+		use("JoosepAlviste/nvim-ts-context-commentstring")
+
+		-- function doc
+		use({
+			"kkoomen/vim-doge",
+			run = ":call doge#install()",
+		})
+
+		-- Display image in editor
+		-- use({ "edluffy/hologram.nvim" })
+
+		-- MDX
+		use("jxnblk/vim-mdx-js")
+
+		if packer_bootstrap then
+			require("packer").sync()
+		end
+	end,
+	config = {
+		-- 锁定插件版本在snapshots目录
+		-- snapshot_path = require("packer.util").join_paths(vim.fn.stdpath("config"), "snapshots"),
+		-- 这里锁定插件版本在v1，不会继续更新插件
+		-- snapshot = "v1",
+
+		-- 最大并发数
+		max_jobs = 16,
+		-- 自定义源
+		git = {
+			-- default_url_format = "https://hub.fastgit.xyz/%s",
+			-- default_url_format = "https://mirror.ghproxy.com/https://github.com/%s",
+			-- default_url_format = "https://gitcode.net/mirrors/%s",
+			-- default_url_format = "https://gitclone.com/github.com/%s",
 		},
-	})
-	-- use 'nvim-telescope/telescope-file-browser.nvim'
-	use("windwp/nvim-autopairs")
-	use("windwp/nvim-ts-autotag")
-	use("norcalli/nvim-colorizer.lua")
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	})
-
-	use("akinsho/nvim-bufferline.lua")
-
-	-- git intergation
-	use("lewis6991/gitsigns.nvim")
-
-	-- File explorer
-	use({
-		"kyazdani42/nvim-tree.lua",
-		requires = {
-			"kyzadani42/nvim-web-devicons",
-		},
-		tag = "nightly",
-	})
-
-	-- Motion
-	use({
-		"phaazon/hop.nvim",
-		branch = "v2", -- optional but strongly recommended
-		config = function()
-			-- you can configure Hop the way you like here; see :h hop-config
-			require("hop").setup({ keys = "etovxqpdygfblzhckisuran" })
-		end,
-	})
-
-	use({ "glepnir/dashboard-nvim" })
-
-	-- multi cursor
-	use("mg979/vim-visual-multi")
-
-	-- Comment
-	use("terrortylor/nvim-comment")
-	use("JoosepAlviste/nvim-ts-context-commentstring")
-
-	-- function doc
-	use({
-		"kkoomen/vim-doge",
-		run = ":call doge#install()",
-	})
-
-	-- Display image in editor
-	-- use({ "edluffy/hologram.nvim" })
-
-	-- MDX
-	use("jxnblk/vim-mdx-js")
-
-	if packer_bootstrap then
-		require("packer").sync()
-	end
-end)
+		-- display = {
+		-- 使用浮动窗口显示
+		--   open_fn = function()
+		--     return require("packer.util").float({ border = "single" })
+		--   end,
+		-- },
+	},
+})
