a62837adbde460dab060e4f9db9d2cb97b1d72da
DefectingCat <i@defect.ink>
2022-11-07 10:37:54 +0800

    Update eslint config

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -38,6 +38,22 @@ ts.setup({
 	context_commentstring = {
 		enable = true,
 	},
+	-- p00f/nvim-ts-rainbow
+	rainbow = {
+		enable = true,
+		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		colors = {
+			"#95ca60",
+			"#ee6985",
+			"#D6A760",
+			"#7794f4",
+			"#b38bf5",
+			"#7cc7fe",
+		}, -- table of hex strings
+		-- termcolors = { } -- table of colour name strings
+	},
 })
 
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
