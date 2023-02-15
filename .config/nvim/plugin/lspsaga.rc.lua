e6e9f8a459c6f7f3caa5d65e5b46d5117e553acd
xfy <i@rua.plus>
2023-02-15 09:11:38 +0800

    Update lua LSP.

--- .config/nvim/plugin/lspsaga.rc.lua
+++ .config/nvim/plugin/lspsaga.rc.lua
@@ -3,11 +3,11 @@ if not status then
 	return
 end
 
-saga.init_lsp_saga({
-	server_filetype_map = {
-		typescript = "typescript",
-	},
-})
+-- saga.init_lsp_saga({
+-- 	server_filetype_map = {
+-- 		typescript = "typescript",
+-- 	},
+-- })
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
