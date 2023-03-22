2f260128c3534ba5675202b90cf9e67bb0f08020
DefectingCat <i@defect.ink>
2023-03-22 13:56:44 +0800

    Add neoscroll

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -1,4 +1,14 @@
 return {
-  "mg979/vim-visual-multi",
-  "jxnblk/vim-mdx-js",
+  {
+    "mg979/vim-visual-multi",
+  },
+  {
+    "jxnblk/vim-mdx-js",
+  },
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup()
+    end,
+  },
 }
