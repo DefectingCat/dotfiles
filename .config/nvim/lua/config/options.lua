ab6c240035eabc0cff19cbb82df6b61a150e9863
xfy <i@rua.plus>
2023-03-30 13:54:29 +0800

    Update neovide copy and paste

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,6 +9,14 @@ opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldlevel = 20
 opt.ignorecase = true
+opt.wildignore:append({ "*/node_modules/*" })
+opt.clipboard:append({ "unnamedplus" })
 
 -- neovide
 opt.guifont = "JetBrainsMono Nerd Font:h16"
+-- Allow clipboard copy paste in neovim
+vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
