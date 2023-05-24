2e45d7a9f213b3bccb322101fae59284903a6ebc
xfy <i@rua.plus>
2023-05-24 16:14:02 +0800

    update neovim config

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -9,7 +9,7 @@ return {
     "echasnovski/mini.bufremove",
     -- stylua: ignore
     keys = {
-      { "<leader>d", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
+      { "<C-w>", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
     },
   },
   {
