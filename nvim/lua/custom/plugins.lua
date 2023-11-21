d27e764a5eea5ec34d304831945b6a14757d87d7
DefectingCat <i@defect.ink>
2023-11-21 16:09:10 +0800

    update comment string in jsx file

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -221,25 +221,38 @@ local plugins = {
   },
   {
     "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup {}
-    end,
   },
   {
     "windwp/nvim-ts-autotag",
-    config = function()
-      require("nvim-ts-autotag").setup {}
-    end,
   },
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup {
-        -- Configuration here, or leave empty to use defaults
-      }
-    end,
+  },
+  -- comment string
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      pre_hook = function(ctx)
+        local U = require "Comment.utils"
+
+        local location = nil
+        if ctx.ctype == U.ctype.block then
+          location = require("ts_context_commentstring.utils").get_cursor_location()
+        elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+          location = require("ts_context_commentstring.utils").get_visual_start_location()
+        end
+
+        return require("ts_context_commentstring.internal").calculate_commentstring {
+          key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+          location = location,
+        }
+      end,
+    },
   },
   -- term
   {
