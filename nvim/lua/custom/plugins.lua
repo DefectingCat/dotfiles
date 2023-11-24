b4a6520d629f0d8c169f1975675dec7a0a13596c
DefectingCat <i@defect.ink>
2023-11-24 10:15:36 +0800

    update nvim ui

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -139,12 +139,6 @@ local plugins = {
   {
     "b0o/schemastore.nvim",
   },
-  {
-    "folke/trouble.nvim",
-    cmd = { "TroubleToggle", "Trouble", "TroubleRefresh" },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {},
-  },
 
   -- debug
   {
@@ -281,12 +275,56 @@ local plugins = {
   },
   -- ui
   {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = { "TroubleToggle", "Trouble", "TroubleRefresh" },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+  },
+  {
     "NvChad/nvim-colorizer.lua",
     opts = {
       user_default_options = {
         tailwind = true,
       },
     },
+  },
+  {
+    "folke/todo-comments.nvim",
+    event = "BufReadPost",
+  },
+  {
+    "chikko80/error-lens.nvim",
+    event = "BufRead",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {},
   },
   -- git
   {
