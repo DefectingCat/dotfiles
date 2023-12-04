3f264ea405f62b4996b79e6a0100f9083bcd4f21
xfy <i@rua.plus>
2023-12-04 11:55:48 +0800

    update macos update script

--- scripts/update-macos.sh
+++ scripts/update-macos.sh
@@ -6,3 +6,24 @@ backup_folder ./zsh/zshrc ~/.zshrc
 
 # neovim
 backup_folder ./nvim/lua/custom ~/.config/nvim/lua/ ~/.config/nvim/lua/custom/
+
+# kitty
+backup_folder ./kitty ~/.config/
+
+# neofetch
+backup_folder ./neofetch ~/.config/ ~/.config/neofetch/
+
+# ranger
+backup_folder ./ranger ~/.config/ ~/.config/ranger/
+
+# lazygit
+backup_folder ./lazygit ~/.config/ ~/.config/lazygit/
+
+# yabai
+backup_folder ./yabai ~/.config/ ~/.config/yabai/
+
+# skhd
+backup_folder ./skhd ~/.config/ ~/.config/skhd/
+
+# sketchybar
+backup_folder ./sketchybar ~/.config/ ~/.config/sketchybar
