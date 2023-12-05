117d59fb7a715afb11ed865487944b2e2b9686bd
xfy <i@rua.plus>
2023-12-05 16:56:55 +0800

    fix backup folder
    if target not exist, do not delete source

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -2,11 +2,11 @@
 
 # $1 source $2 target $3 remove old
 function backup_folder() {
-	if [ "$3" ]; then
-		rm -rf "$3"
-	fi
-
 	if [ -r "$1" ]; then
+		if [ "$3" ]; then
+			rm -rf "$3"
+		fi
+
 		echo "straing copy $1 to $2"
 		cp -aR "$1" "$2"
 	else
