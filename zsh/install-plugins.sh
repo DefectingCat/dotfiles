2fa9fa06ebf5e3e776e1755ef17ecb5f29c004e4
xfy <i@rua.plus>
2023-12-05 13:12:33 +0800

    update install zsh plugin
    add update if plugin installed

--- zsh/install-plugins.sh
+++ zsh/install-plugins.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-#
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
-git clone https://github.com/jeffreytse/zsh-vi-mode "$ZSH_CUSTOM"/plugins/zsh-vi-mode
+
+# $1 git path $2 target directory
+function install_plugin {
+	if [ ! -d "$2" ]; then
+		echo "install $1"
+		git clone --depth=1 "$1" "$2"
+	else
+		cd "$2" || exit
+		echo "update $1"
+		git pull
+	fi
+}
+
+install_plugin https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+install_plugin https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+install_plugin https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/themes/powerlevel10k
+install_plugin https://github.com/jeffreytse/zsh-vi-mode "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-vi-mode
 echo "Done."
