fc9155e4e6582fd6cb92ae7eb8cefc667a1aa549
DefectingCat <i@defect.ink>
2023-12-08 09:28:44 +0800

    add btop config backup

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,14 +6,17 @@ backup_folder ~/.config/nvim/lua/custom ./nvim/lua/ ./nvim/lua/custom/
 backup_folder ~/.config/neofetch ./ ./neofetch/
 # zshrc
 backup_folder ~/.zshrc ./zsh/zshrc
+backup_folder ~/.p10k.zsh ./zsh/p10k.zsh
 # ranger
 backup_folder ~/.config/ranger ./ ./ranger/
 # tmux
 backup_folder ~/.tmux/tmux.conf ./tmux/ ./tmux/tmux.conf
 # bash scripts
-backup_folder ~/.config/rua-scripts ./
+backup_folder ~/.config/rua-scripts ./ ./rua-scripts
 # lazygit
 backup_folder ~/.config/lazygit ./ ./lazygit/
+# btop
+backup_folder ~/.config/btop ./ ./btop
 
 if [[ $OSTYPE == "linux"* ]]; then
 	# hyprland
