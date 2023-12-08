86ed7041d8c83adc6232deb5355bfd3ded11ecc2
DefectingCat <i@defect.ink>
2023-12-08 09:24:47 +0800

    update backup script format

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
 source ./scripts/bash.sh
-
 # neovim
 backup_folder ~/.config/nvim/lua/custom ./nvim/lua/ ./nvim/lua/custom/
+# neofetch
+backup_folder ~/.config/neofetch ./ ./neofetch/
+# zshrc
+backup_folder ~/.zshrc ./zsh/zshrc
+# ranger
+backup_folder ~/.config/ranger ./ ./ranger/
+# tmux
+backup_folder ~/.tmux/tmux.conf ./tmux/ ./tmux/tmux.conf
+# bash scripts
+backup_folder ~/.config/rua-scripts ./
+# lazygit
+backup_folder ~/.config/lazygit ./ ./lazygit/
+
+if [[ $OSTYPE == "linux"* ]]; then
+	# hyprland
+	backup_folder ~/.config/hypr ./ ./hypr
+fi
 
 if [[ $OSTYPE == "darwin"* ]]; then
 	# kitty
 	backup_folder ~/.config/kitty ./ ./kitty/
-fi
-
-# neofetch
-backup_folder ~/.config/neofetch ./ ./neofetch/
-
-# zshrc
-backup_folder ~/.zshrc ./zsh/zshrc
-
-# ranger
-backup_folder ~/.config/ranger ./ ./ranger/
-
-# tmux
-backup_folder ~/.tmux/tmux.conf ./tmux/ ./tmux/tmux.conf
-
-# bash scripts
-backup_folder ~/.config/rua-scripts ./
-
-# lazygit
-backup_folder ~/.config/lazygit ./ ./lazygit/
-
-# hyprland
-backup_folder ~/.config/hypr ./ ./hypr
-
-if [[ $OSTYPE == "darwin"* ]]; then
 	# yabai
 	backup_folder ~/.config/yabai ./ ./yabai/
-
 	# skhd
 	backup_folder ~/.config/skhd ./ ./skhd/
-
 	# sketchybar
 	backup_folder ~/.config/sketchybar ./ ./sketchybar/
 fi
