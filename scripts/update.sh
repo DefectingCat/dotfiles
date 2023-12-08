8703a68619ef19491a8cf2467a2f259174b7bfed
xfy <i@rua.plus>
2023-12-08 09:28:44 +0800

    add btop config backup

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 source ./scripts/bash.sh
-
 # zsh
 backup_folder ./zsh/zshrc ~/.zshrc
-
 # neovim
 backup_folder ./nvim/lua/custom ~/.config/nvim/lua/ ~/.config/nvim/lua/custom/
-
-# kitty
-if [[ $OSTYPE == "darwin"* ]]; then
-	backup_folder ./kitty ~/.config/
-fi
-
 # neofetch
 backup_folder ./neofetch ~/.config/ ~/.config/neofetch/
-
 # ranger
 backup_folder ./ranger ~/.config/ ~/.config/ranger/
-
 # lazygit
 backup_folder ./lazygit ~/.config/ ~/.config/lazygit/
-
 # bash scripts
 backup_folder ./rua-scripts ~/.config/
+# btop
+backup_folder ./btop ~/.config/
+
+if [[ $OSTYPE == "linux"* ]]; then
+	# hyprland
+	backup_folder ./hypr ~/.config/
+fi
 
 if [[ $OSTYPE == "darwin"* ]]; then
+	# kitty
+	backup_folder ./kitty ~/.config/
 	# yabai
 	backup_folder ./yabai ~/.config/ ~/.config/yabai/
 	# skhd
