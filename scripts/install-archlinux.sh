82218d187fe080873362ee4e4b04ba334d33d48a
DefectingCat <i@defect.ink>
2023-12-08 13:12:07 +0800

    update arch-dev

--- scripts/install-archlinux.sh
+++ scripts/install-archlinux.sh
@@ -18,12 +18,6 @@ chmod +x /home/"$USER"/dotfiles/zsh/install-plugins.sh
 # neovim
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 echo "install neovim done"
-# nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-export NVM_DIR="/home/$USER/.nvm"
-\. "$NVM_DIR/nvm.sh"
-nvm install --lts
-echo "install nvm done"
 # rustc
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 echo "install rustup done"
