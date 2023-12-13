bd18539082e9535577e56ea5b6b5064f7d3f12b4
DefectingCat <i@defect.ink>
2023-12-13 13:18:18 +0800

    change neovide setting

--- nvim/lua/custom/utils/neovide.lua
+++ nvim/lua/custom/utils/neovide.lua
@@ -8,7 +8,7 @@ end
 if vim.g.neovide then
   opt.guifont = "JetBrainsMono Nerd Font:h16"
   -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
-  vim.g.neovide_transparency = 0.0
+  vim.g.neovide_transparency = 0
   vim.g.transparency = 0.8
   vim.g.neovide_background_color = "#0f1117" .. alpha()
   vim.g.neovide_floating_blur_amount_x = 2.0
