c1fc7baad27aca2e0ada1af45b78530a448e2f53
xfy <i@rua.plus>
2023-12-13 10:06:44 +0800

    add neovide config

---
+++ nvim/lua/custom/utils/neovide.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+-- Helper function for transparency formatting
+local alpha = function()
+  return string.format("%x", math.floor(255 * (vim.g.transparency or 0.8)))
+end
+
+if vim.g.neovide then
+  opt.guifont = "JetBrainsMono Nerd Font:h16"
+  -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+  vim.g.neovide_transparency = 0.0
+  vim.g.transparency = 0.8
+  vim.g.neovide_background_color = "#0f1117" .. alpha()
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_refresh_rate = 120
+  vim.g.neovide_confirm_quit = true
+  vim.g.neovide_input_macos_alt_is_meta = true
+end
