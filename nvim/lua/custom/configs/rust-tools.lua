41d596e72d07c24086c18a66d21fcb76f98323fa
xfy <i@rua.plus>
2023-12-15 10:04:28 +0800

    add lldb config for rust-tools

--- nvim/lua/custom/configs/rust-tools.lua
+++ nvim/lua/custom/configs/rust-tools.lua
@@ -1,6 +1,11 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
+local mason_registry = require "mason-registry"
+local codelldb_root = mason_registry.get_package("codelldb"):get_install_path() .. "/extension/"
+local codelldb_path = codelldb_root .. "adapter/codelldb"
+local liblldb_path = codelldb_root .. "lldb/lib/liblldb.so"
+
 local options = {
   server = {
     on_attach = on_attach,
@@ -19,6 +24,9 @@ local options = {
         enable = true,
       },
     },
+  },
+  dap = {
+    adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
   },
 }
 
