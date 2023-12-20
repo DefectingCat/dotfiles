6841e1ba715275514cdf175bee5967b75e42b01f
xfy <i@rua.plus>
2023-12-20 09:07:35 +0800

    update arch dev dockerfile
    remove old files

--- scripts/install-archlinux.sh
+++ scripts/install-archlinux.sh
@@ -5,6 +5,7 @@ cd /home/"$USER"/ || exit
 git clone https://aur.archlinux.org/yay.git
 cd /home/"$USER"/yay || exit
 makepkg -si --noconfirm
+cd /home/"$USER"/ || exit
 rm -rf yay
 echo "install yay success"
 
