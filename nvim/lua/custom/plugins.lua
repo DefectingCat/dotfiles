7a43fc44787e866c43e934ad2fa77434347ae946
xfy <i@rua.plus>
2023-12-27 09:43:56 +0800

    add condition disable highlight

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -92,6 +92,15 @@ local plugins = {
       autotag = {
         enable = true,
       },
+      highlight = {
+        disable = function(_, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
     },
   },
   {
