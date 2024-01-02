075fbd50e3f762dfeedb971fece87d0aeac58ddd
xfy <i@rua.plus>
2024-01-02 09:34:20 +0800

    update rust tools configruation

--- nvim/lua/custom/configs/rust-tools.lua
+++ nvim/lua/custom/configs/rust-tools.lua
@@ -2,9 +2,17 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local mason_registry = require "mason-registry"
-local codelldb_root = mason_registry.get_package("codelldb"):get_install_path() .. "/extension/"
-local codelldb_path = codelldb_root .. "adapter/codelldb"
-local liblldb_path = codelldb_root .. "lldb/lib/liblldb.so"
+local codelldb = mason_registry.get_package "codelldb"
+local extension_path = codelldb:get_install_path() .. "/extension/"
+local codelldb_path = extension_path .. "adapter/codelldb"
+local liblldb_path = ""
+if vim.loop.os_uname().sysname:find "Windows" then
+  liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
+elseif vim.fn.has "mac" == 1 then
+  liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+else
+  liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+end
 
 local options = {
   server = {
@@ -12,8 +20,15 @@ local options = {
     capabilities = capabilities,
     settings = {
       ["rust-analyzer"] = {
+        cargo = {
+          allFeatures = true,
+          loadOutDirsFromCheck = true,
+          runBuildScripts = true,
+        },
         checkOnSave = {
+          allFeatures = true,
           command = "clippy",
+          extraArgs = { "--no-deps" },
         },
       },
       check = {
@@ -22,11 +37,27 @@ local options = {
       },
       procMacro = {
         enable = true,
+        ignored = {
+          ["async-trait"] = { "async_trait" },
+          ["napi-derive"] = { "napi" },
+          ["async-recursion"] = { "async_recursion" },
+        },
       },
     },
   },
   dap = {
     adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+  },
+  tools = {
+    on_initialized = function()
+      vim.cmd [[
+              augroup RustLSP
+                autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
+                autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
+                autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
+              augroup END
+            ]]
+    end,
   },
 }
 
