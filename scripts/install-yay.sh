3f63c3509fd5e3617a94719723f17b77b48a9faf
DefectingCat <i@defect.ink>
2024-01-03 10:12:17 +0800

    encapsluate install yay function

---
+++ scripts/install-yay.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+install_yay() {
+	cd /home/"$USER"/ || exit
+	git clone https://aur.archlinux.org/yay.git
+	cd /home/"$USER"/yay || exit
+	makepkg -si --noconfirm
+	cd /home/"$USER"/ || exit
+	rm -rf yay
+	echo "install yay success"
+}
