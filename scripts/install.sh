eea67214eec102ab455a65723bd6d9fc46517e44
xfy <i@rua.plus>
2024-02-23 09:16:09 +0800

    change folder structure

---
+++ scripts/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# create symbol link for configurations
+
+# nvim
+ln -s ~/dotfiles/nvim/custom/ ~/.config/nvim/lua/
+# zsh
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+# tmux
+ln -s ~/dotfiles/tmux/ ~/.tmux
+# scripts
+ln -s ~/dotfiles/rua-scripts/ ~/.config/
+# lazygit
+ln -s ~/dotfiles/lazygit/ ~/.config/
+# neofetch
+ln -s ~/dotfiles/neofetch/ ~/.config/
+# kitty
+ln -s ~/dotfiles/kitty/ ~/.config/
