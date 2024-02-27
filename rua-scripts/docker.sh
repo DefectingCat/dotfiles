0b80b2ef581881b21510bb5892a7849a15b341d7
xfy <i@rua.plus>
2024-02-27 15:28:34 +0800

    update gitignore
    add custom alias

---
+++ rua-scripts/docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+alias docker_build="docker build -t ${IMAGE_NAME} --progress=plain ."
+alias docker_up="docker build -t ${IMAGE_NAME} --build-arg COMMAND=${BUILD_COMMAND} --progress=plain . && docker tag $IMAGE_NAME 192.168.1.57:8004/$IMAGE_NAME && docker push 192.168.1.57:8004/$IMAGE_NAME"
