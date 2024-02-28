16e210453b823ab642e27a97f1e4e33f1d48edb4
DefectingCat <i@defect.ink>
2024-02-28 17:27:49 +0800

    update docker build command

--- rua-scripts/docker.sh
+++ rua-scripts/docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 alias docker_build="docker build -t ${IMAGE_NAME} --progress=plain ."
-alias docker_up="docker build -t ${IMAGE_NAME} --build-arg COMMAND=${BUILD_COMMAND} --progress=plain . && docker tag $IMAGE_NAME 192.168.1.57:8004/$IMAGE_NAME && docker push 192.168.1.57:8004/$IMAGE_NAME"
+# alias docker_up="docker build -t ${IMAGE_NAME} --build-arg COMMAND=${BUILD_COMMAND} --progress=plain . && docker tag $IMAGE_NAME 192.168.1.57:8004/$IMAGE_NAME && docker push 192.168.1.57:8004/$IMAGE_NAME"
+alias docker_up="docker build -t ${IMAGE_NAME} --progress=plain . && docker tag $IMAGE_NAME 192.168.1.57:8004/$IMAGE_NAME && docker push 192.168.1.57:8004/$IMAGE_NAME"
