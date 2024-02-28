316377a5b16a607842a238c1f6f267f9fe04269a
DefectingCat <i@defect.ink>
2024-02-28 08:38:18 +0800

    add ranger config

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,16 +3,18 @@
 # create symbol link for configurations
 
 # nvim
-ln -s ~/dotfiles/nvim/custom/ ~/.config/nvim/lua/
+ln -s ~/dotfiles/nvim/custom/ ~/.config/nvim/lua
 # zsh
 ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
 # tmux
 ln -s ~/dotfiles/tmux/ ~/.tmux
 # scripts
-ln -s ~/dotfiles/rua-scripts/ ~/.config/
+ln -s ~/dotfiles/rua-scripts/ ~/.config
 # lazygit
-ln -s ~/dotfiles/lazygit/ ~/.config/
+ln -s ~/dotfiles/lazygit/ ~/.config
 # neofetch
-ln -s ~/dotfiles/neofetch/ ~/.config/
+ln -s ~/dotfiles/neofetch/ ~/.config
 # kitty
-ln -s ~/dotfiles/kitty/ ~/.config/
+ln -s ~/dotfiles/kitty/ ~/.config
+# ranger
+ln -s ~/dotfiles/ranger/ ~/.config
