d57dd01f1122e92f7dca36cc65321ec97de807f4
xfy <i@rua.plus>
2024-04-15 16:31:03 +0800

    update custom scripts

--- rua-scripts/docker.sh
+++ rua-scripts/docker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-alias docker_build="docker build -t ${IMAGE_NAME} --progress=plain ."
+# alias docker_build="docker build -t ${IMAGE_NAME} --progress=plain ."
 # alias docker_up="docker build -t ${IMAGE_NAME} --build-arg COMMAND=${BUILD_COMMAND} --progress=plain . && docker tag $IMAGE_NAME 192.168.1.57:8004/$IMAGE_NAME && docker push 192.168.1.57:8004/$IMAGE_NAME"
-alias docker_up="docker build -t ${IMAGE_NAME} --progress=plain . && docker tag $IMAGE_NAME 192.168.1.57:8004/$IMAGE_NAME && docker push 192.168.1.57:8004/$IMAGE_NAME"
+# alias docker_up="docker build -t ${IMAGE_NAME} --progress=plain . && docker tag $IMAGE_NAME 192.168.1.57:8004/$IMAGE_NAME && docker push 192.168.1.57:8004/$IMAGE_NAME"
+
+docker_build() {
+	docker build -t ${IMAGE_NAME} --progress=plain .
+}
+
+docker_up() {
+	docker build -t ${IMAGE_NAME} --progress=plain . && docker tag $IMAGE_NAME 192.168.1.57:8004/$IMAGE_NAME && docker push 192.168.1.57:8004/$IMAGE_NAME
+}
