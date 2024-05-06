316c6bc02b6463517406d67d9046f2e5a783d336
xfy <i@rua.plus>
2024-05-06 14:59:08 +0800

    update build and upload

--- rua-scripts/show-client.sh
+++ rua-scripts/show-client.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xe
+
 ## 16 3.0 root
 build-v3-editor() {
 	nvm use && yarn && yarn build-v3 && scp -rp build/* root@192.168.1.16:/home/wwwroot/exhibitor/editormeta
@@ -16,4 +18,15 @@ build-v2-editor() {
 
 build-v2-viewer() {
 	nvm use && yarn && yarn buildviewer && scp -rp build/* root@192.168.1.16:/home/wwwroot/design
+}
+
+## 13 2.0
+build-13-v2-editor() {
+	nvm use && yarn && yarn build && sshpass -f ~/passwd/13.txt scp -r build/* exhibitor@192.168.1.13:/editor || exit 1
+	echo "Upload done"
+}
+
+build-13-v2-viewer() {
+	nvm use && yarn && yarn buildviewer && sshpass -f ~/passwd/13.txt scp -r build/* design@192.168.1.13:/ || exit 1
+	echo "Upload done"
 }
