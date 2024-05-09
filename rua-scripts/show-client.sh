65efa52fd91c1a84a10347d2604db3f016c45196
xfy <i@rua.plus>
2024-05-09 11:28:04 +0800

    add build command

--- rua-scripts/show-client.sh
+++ rua-scripts/show-client.sh
@@ -32,3 +32,14 @@ build-13-v2-viewer() {
 	nvm use && yarn && yarn buildviewer && sshpass -f ~/passwd/13.txt scp -r build/* design@192.168.1.13:/ || exit 1
 	echo "Upload done"
 }
+
+## 羚羊 2.0
+build-ly-v2-editor() {
+	nvm use && yarn && yarn buildlingyang && sshpass -f ~/passwd/lingyang.txt scp -P 106 -r build/* exhibitor@124.70.207.176:/editor || exit 1
+	echo "Upload done"
+}
+
+build-ly-v2-viewer() {
+	nvm use && yarn && yarn generate-lingyang && sshpass -f ~/passwd/lingyang.txt scp -P 106 -r build/* design@124.70.207.176:/ || exit 1
+	echo "Upload done"
+}
