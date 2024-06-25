e6b6bd55bd965c917973ddb0899a530bcf7b637a
xfy <i@rua.plus>
2024-06-25 17:34:34 +0800

    update dev container ssh agent

--- dockerfiles/ubuntu/build.sh
+++ dockerfiles/ubuntu/build.sh
@@ -1,6 +1,5 @@
 #!/bin/env bash
 
 docker build --progress=plain \
-	--build-arg USER_ID=$(id -u $USER) \
-	--build-arg GROUP_ID=$(id -g $USER) \
+	--build-arg USER=$(whoami) \
 	-t ubuntu-dev -f dockerfiles/ubuntu/Dockerfile .
