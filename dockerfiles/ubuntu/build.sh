e52bfb820e933acd62e1b99bdf6b201eca2c00dc
xfy <i@rua.plus>
2024-10-14 17:35:10 +0800

    update ubuntu dev dockerfile

--- dockerfiles/ubuntu/build.sh
+++ dockerfiles/ubuntu/build.sh
@@ -1,5 +1,11 @@
 #!/bin/env bash
 
+set -xe
+
+echo $(whoami)
+
 docker build --progress=plain \
-	--build-arg USER=$(whoami) \
-	-t ubuntu-dev .
+    --build-arg UID=$(id -u) \
+    --build-arg GID=$(id -g) \
+    --build-arg USER=$(whoami) \
+    -t ubuntu-dev .
--- dockerfiles/ubuntu/start.sh
+++ dockerfiles/ubuntu/start.sh
@@ -1,5 +1,24 @@
 #!/bin/env bash
 
+set -xe
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <home folder> <project folder>"
+    exit 1
+fi
+
+HOME_FOLDER=$1
+PROJECT_FOLDER=$2
+
+if [ -z "$(ls -A "$HOME_FOLDER")" ]; then
+    docker run --rm -itd --name=ubuntu-dev ubuntu-dev || exit 1
+    docker cp ubuntu-dev:/home/xfy/. $HOME_FOLDER || docker stop ubuntu-dev
+    docker stop ubuntu-dev
+fi
+
 docker run --rm -it --name=ubuntu-dev \
-	-v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent \
-	ubuntu-dev
+    --user xfy:xfy \
+    -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent \
+    -v $HOME_FOLDER:/home/xfy \
+    -v $PROJECT_FOLDER:/app \
+    ubuntu-dev # --user $(id -u):$(id -g) \
