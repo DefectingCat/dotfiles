b5f95e650bc3121a534ff5a67d06cfeb9d758e11
xfy <xfyxfy@proton.me>
2024-10-22 15:58:01 +0800

    restore ubuntu dockerfile

--- dockerfiles/ubuntu/build.sh
+++ dockerfiles/ubuntu/build.sh
@@ -4,8 +4,9 @@ set -xe
 
 echo $(whoami)
 
+# --build-arg UID=$(id -u) \
+# --build-arg GID=$(id -g) \
+
 docker build --progress=plain \
-    --build-arg UID=$(id -u) \
-    --build-arg GID=$(id -g) \
     --build-arg USER=$(whoami) \
     -t ubuntu-dev .
--- dockerfiles/ubuntu/start.sh
+++ dockerfiles/ubuntu/start.sh
@@ -7,18 +7,19 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-HOME_FOLDER=$1
-PROJECT_FOLDER=$2
+# HOME_FOLDER=$1
+PROJECT_FOLDER=$1
 
-if [ -z "$(ls -A "$HOME_FOLDER")" ]; then
-    docker run --rm -itd --name=ubuntu-dev ubuntu-dev || exit 1
-    docker cp ubuntu-dev:/home/xfy/. $HOME_FOLDER || docker stop ubuntu-dev
-    docker stop ubuntu-dev
-fi
+# if [ -z "$(ls -A "$HOME_FOLDER")" ]; then
+#     docker run --rm -itd --name=ubuntu-dev ubuntu-dev || exit 1
+#     docker cp ubuntu-dev:/home/xfy/. $HOME_FOLDER || docker stop ubuntu-dev
+#     docker stop ubuntu-dev
+# fi
+
+# -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent
+# -v $HOME_FOLDER:/home/xfy \
 
 docker run --rm -it --name=ubuntu-dev \
     --user xfy:xfy \
-    -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent \
-    -v $HOME_FOLDER:/home/xfy \
     -v $PROJECT_FOLDER:/app \
-    ubuntu-dev # --user $(id -u):$(id -g) \
+    ubuntu-dev
