351b5f407e5491aceba6270b1f123c73baf832d0
xfy <i@rua.plus>
2024-11-05 15:02:49 +0800

    update kitty icons
    update wezterm configuration

--- wezterm/macos/wezterm.lua
+++ wezterm/macos/wezterm.lua
@@ -13,10 +13,26 @@ config.window_padding = {
     top = 0,
     bottom = 0,
 }
+
+-- window
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.97
+config.window_background_opacity = 0.96
+config.macos_window_background_blur = 10
+config.initial_rows = 30
+config.initial_cols = 90
+
+-- tab bar
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.colors = {
+    tab_bar = {
+        background = "rgba(0,0,0,0)"
+    },
+}
+
 config.hide_tab_bar_if_only_one_tab = true
 config.color_scheme = "Catppuccin Mocha"
 config.font_size = 16
+
 
 return config
