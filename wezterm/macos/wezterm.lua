a79ef27e6eaf06efc6fb80b94c2ceb470e0573dd
xfy <i@rua.plus>
2025-04-24 10:30:13 +0800

    change font

--- wezterm/macos/wezterm.lua
+++ wezterm/macos/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- config.font = wezterm.font("JetBrains Mono NL")
-config.font = wezterm.font_with_fallback({ "JetBrains Mono NL", "PingFang SC" })
+config.font = wezterm.font_with_fallback({ "Maple Mono Normal NF CN", "PingFang SC" })
 
 -- ui
 --[[ config.initial_rows = 50 ]]
@@ -19,8 +19,8 @@ config.window_padding = {
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 1
 config.macos_window_background_blur = 37
-config.initial_rows = 30
-config.initial_cols = 90
+config.initial_rows = 33
+config.initial_cols = 120
 
 -- tab bar
 config.use_fancy_tab_bar = false
@@ -31,8 +31,8 @@ config.colors = {
     },
 }
 
-config.hide_tab_bar_if_only_one_tab = true
-config.color_scheme = "Catppuccin Mocha"
+-- config.hide_tab_bar_if_only_one_tab = true
+-- config.color_scheme = "Catppuccin Mocha"
 config.font_size = 16
 
 config.mouse_bindings = {
