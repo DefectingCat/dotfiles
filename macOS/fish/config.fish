2b2b2dd984d0dd93e33b4675771cd61e71734e33
xfy <i@rua.plus>
2025-08-31 21:44:30 +0800

    create macos fish directory

---
+++ macOS/fish/config.fish
@@ -0,0 +1,32 @@
+# source /usr/share/cachyos-fish-config/cachyos-config.fish
+source $HOME/.config/fish/cachyos.fish
+source $HOME/.config/fish/git.fish
+
+starship init fish | source
+
+alias vim="nvim"
+alias icat="kitty +kitten icat"
+alias kssh="kitty +kitten ssh"
+# alias ll="ls -lhtr"
+alias ll="lsd -lhtrF"
+alias vi="/usr/bin/vim"
+alias vim="nvim"
+alias vim-update='nvim --headless "+Lazy! update" +qa'
+# alias nvim-update='nvim --headless "+Lazy! sync" +qa'
+alias x86="env /usr/bin/arch -x86_64 /bin/zsh --login"
+alias arm="env /usr/bin/arch -arm64 /bin/zsh --login"
+alias dur="du -h -d 1 2>/dev/null | sort -hr"
+
+set -Ux EDITOR nvim
+set -Ux http_proxy "http://127.0.0.1:7890"
+set -Ux https_proxy "http://127.0.0.1:7890"
+set -Ux LANG "en_US.UTF-8"
+set -Ux LC_ALL "en_US.UTF-8"
+set -Ux COLORTERM truecolor
+set -Ux TERM xterm-256color
+
+# overwrite greeting
+# potentially disabling fastfetch
+# function fish_greeting
+#    # smth smth
+# end
