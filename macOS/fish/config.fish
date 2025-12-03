cc7082a95c4d8113107fa216efed71f18d3fe377
xfy <i@rua.plus>
2025-12-03 10:01:04 +0800

    update fish config

--- macOS/fish/config.fish
+++ macOS/fish/config.fish
@@ -18,11 +18,20 @@ alias arm="env /usr/bin/arch -arm64 /bin/zsh --login"
 alias dur="du -h -d 1 2>/dev/null | sort -hr"
 
 set -Ux EDITOR nvim
-set -Ux http_proxy "http://127.0.0.1:7890"
-set -Ux https_proxy "http://127.0.0.1:7890"
+# set -Ux http_proxy "socks5://127.0.0.1:13659"
+# set -Ux https_proxy "socks5://127.0.0.1:13659"
+set -Ux http_proxy "http://127.0.0.1:10808"
+set -Ux https_proxy "http://127.0.0.1:10808"
+set -Ux RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
+set -Ux RUSTUP_UPDATE_ROOT https://mirrors.ustc.edu.cn/rust-static/rustup
+set -Ux RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
+set -Ux RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
 set -Ux LANG "en_US.UTF-8"
 set -Ux LC_ALL "en_US.UTF-8"
 set -Ux COLORTERM truecolor
 set -Ux TERM xterm-256color
 
 starship init fish | source
+
+# Added by Qoder CLI installer
+set -gx PATH $PATH /Users/issuser/.local/bin
