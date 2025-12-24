e98b7cbcc9d36d22834e23dc643c88c0cce21c5c
xfy <i@rua.plus>
2025-12-24 10:44:53 +0800

    update macos fish

---
+++ macOS/fish/private_env_example.fish
@@ -0,0 +1,7 @@
+set -Ux DEEPSEEK_API_KEY
+
+# huoshan
+set -Ux ANTHROPIC_BASE_URL
+set -Ux ANTHROPIC_AUTH_TOKEN
+# 按需选择模型doubao-seed-code-preview-latest、ark-code-latest
+set -Ux ANTHROPIC_MODEL doubao-seed-code-preview-latest
