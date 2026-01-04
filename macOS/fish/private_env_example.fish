d732c0a260c21ac0662b377529cd88c8b665e9cd
xfy <i@rua.plus>
2026-01-04 13:19:59 +0800

    更新配置文件，添加API密钥示例和配置复制脚本

--- macOS/fish/private_env_example.fish
+++ macOS/fish/private_env_example.fish
@@ -1,10 +1,5 @@
-set -Ux DEEPSEEK_API_KEY
-set -Ux DOUBAO_API_KEY
-set -Ux GOOGLE_SEARCH_API_KEY
-set -Ux GOOGLE_SEARCH_ENGINE_ID
-
-# huoshan
-set -Ux ANTHROPIC_BASE_URL
-set -Ux ANTHROPIC_AUTH_TOKEN
-# 按需选择模型doubao-seed-code-preview-latest、ark-code-latest
-set -Ux ANTHROPIC_MODEL doubao-seed-code-preview-latest
+set -Ux DEEPSEEK_API_KEY xxx
+set -Ux DOUBAO_API_KEY xxx
+set -Ux OPENROUTER_API_KEY xxx
+set -Ux GOOGLE_SEARCH_API_KEY xxx
+set -Ux GOOGLE_SEARCH_ENGINE_ID nvim
