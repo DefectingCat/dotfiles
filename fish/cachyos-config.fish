ef605efe0392b081b6ade24f089c7f3120e1fff2
xfy <i@rua.plus>
2026-04-21 14:19:00 +0800

    reorganize fish config: rename cachyos and move done to conf.d
    - Rename cachyos.fish to cachyos-config.fish for clarity
    - Move done.fish to conf.d/ for automatic loading
    - Update config.fish to source the renamed file

--- fish/cachyos-config.fish
+++ fish/cachyos-config.fish
@@ -1,6 +1,5 @@
-## Source from conf.d before our fish config
-# source /usr/share/cachyos-fish-config/conf.d/done.fish
-source $HOME/.config/fish/done.fish
+﻿## Source from conf.d before our fish config
+source $HOME/.config/fish/conf.d/done.fish
 
 ## Set values
 ## Run fastfetch as welcome message
@@ -9,7 +8,7 @@ function fish_greeting
 end
 
 # Format man pages
-set -x MANROFFOPT -c
+set -x MANROFFOPT "-c"
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 # Set settings for https://github.com/franciscolourenco/done
@@ -19,57 +18,44 @@ set -U __done_notification_urgency_level low
 ## Environment setup
 # Apply .profile: use this to put fish compatible .profile stuff in
 if test -f ~/.fish_profile
-    source ~/.fish_profile
+  source ~/.fish_profile
 end
 
-# Add ~/.local/bin to PATH
-if test -d ~/.local/bin
-    if not contains -- ~/.local/bin $PATH
-        set -p PATH ~/.local/bin
-    end
-end
-
-# Add depot_tools to PATH
-if test -d ~/Applications/depot_tools
-    if not contains -- ~/Applications/depot_tools $PATH
-        set -p PATH ~/Applications/depot_tools
-    end
-end
+# Append common directories for executable files to $PATH
+fish_add_path ~/.local/bin ~/.cargo/bin ~/Applications/depot_tools
 
 ## Functions
 # Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
 function __history_previous_command
-    switch (commandline -t)
-        case "!"
-            commandline -t $history[1]
-            commandline -f repaint
-        case "*"
-            commandline -i !
-    end
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
 end
 
 function __history_previous_command_arguments
-    switch (commandline -t)
-        case "!"
-            commandline -t ""
-            commandline -f history-token-search-backward
-        case "*"
-            commandline -i '$'
-    end
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
 end
 
-if [ "$fish_key_bindings" = fish_vi_key_bindings ]
-
-    bind -Minsert ! __history_previous_command
-    bind -Minsert '$' __history_previous_command_arguments
+if [ "$fish_key_bindings" = fish_vi_key_bindings ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
 else
-    bind ! __history_previous_command
-    bind '$' __history_previous_command_arguments
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
 end
 
 # Fish command history
 function history
-    builtin history --show-time='%F %T '
+    builtin history --show-time='%F %T ' $argv
 end
 
 function backup --argument filename
@@ -90,11 +76,11 @@ end
 
 ## Useful aliases
 # Replace ls with eza
-alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
-alias la='eza -a --color=always --group-directories-first --icons' # all files and dirs
-alias ll='eza -l --color=always --group-directories-first --icons' # long format
-alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
-alias l.="eza -a | grep -e '^\.'" # show only dotfiles
+alias ls='eza -al --color=always --group-directories-first --icons=always' # preferred listing
+alias la='eza -a --color=always --group-directories-first --icons=always'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons=always'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons=always' # tree listing
+alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
 
 # Common use
 alias grubup="sudo grub-mkconfig -o /boot/grub/grub.cfg"
@@ -114,10 +100,10 @@ alias vdir='vdir --color=auto'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-alias hw='hwinfo --short' # Hardware Info
-alias big="expac -H M '%m\t%n' | sort -h | nl" # Sort installed packages according to size in MB
-alias gitpkg='pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
-alias update='sudo pacman -Syu'
+alias hw='hwinfo --short'                                   # Hardware Info
+alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB
+alias gitpkg='pacman -Q | grep -i "\-git" | wc -l'          # List amount of -git packages
+alias update='sudo cachyos-rate-mirrors && sudo pacman -Syu'
 
 # Get fastest mirrors
 alias mirror="sudo cachyos-rate-mirrors"
@@ -125,7 +111,6 @@ alias mirror="sudo cachyos-rate-mirrors"
 # Help people new to Arch
 alias apt='man pacman'
 alias apt-get='man pacman'
-alias please='sudo'
 alias tb='nc termbin.com 9999'
 
 # Cleanup orphaned packages
