3da6cddb3bba39c0bcd9131c24d81c74061c7d28
xfy <i@rua.plus>
2026-04-24 16:20:02 +0800

    Remove obsolete scripts and add kitty app icon
    - Delete unused linux scripts (ssh-forward.sh, xdg-startup.sh)
    - Delete project-specific show-client-16.sh
    - Add kitty.app.icns for kitty terminal

--- rua-scripts/linux/ssh-forward.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/env bash
-
-# 16 mysql
-ssh -f -N -L 192.168.1.57:3306:localhost:3306 16
--- rua-scripts/linux/xdg-startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export XDG_RUNTIME_DIR=/tmp/$UID
-if [ ! -d $XDG_RUNTIME_DIR ]; then
-	mkdir /tmp/$UID
-fi
